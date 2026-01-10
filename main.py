05fac3a89e3a91602f5de7bd5be34686b4d2b25e
hadar lavsky <hadarlavsky@gmail.com>
2026-01-10 15:33:57 +0200

    basic model

---
+++ main.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+import torch
+
+def train_swimmer_model():
+    # 1. Load the architecture with pre-trained weights
+    # 'yolov8s-pose.pt' is the 'Small' version—good balance of speed/accuracy
+    model = YOLO('yolov8s-pose.pt') 
+
+    # 2. Connect and Train
+    model.train(
+        data='Dataset/data.yaml',
+        epochs=100,
+        imgsz=640,
+        batch=16,
+        device=0 if torch.cuda.is_available() else 'cpu',
+        project='swimmer_project',
+        name='v1_baseline',
+        save=True
+    )
+
+if __name__ == "__main__":
+    train_swimmer_model()
